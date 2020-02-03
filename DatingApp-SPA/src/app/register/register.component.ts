e0a47c9fe433b380ea6d421aacb99b9aafc11aae
pritama.nayak <pritama.nayak@mindfiresolutions.com>
2020-02-03 18:42:35 +0530

    added alertify for alerts

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,21 +12,20 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   cancel() {
-    console.log('cancelled');
     this.cancelRegister.emit(false);
   }
 
   register() {
     this.authService.register(this.model).subscribe(next => {
-      console.log('success');
+      this.alertify.success('Registration successfull.');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 }
