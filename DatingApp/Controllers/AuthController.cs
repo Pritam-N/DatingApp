6cd70e063a024debc643819bdfe26b99e3ebce8f
Pritama Kumar Nayak <Pritam-N@users.noreply.github.com>
2020-02-12 11:40:12 +0530

    Update AuthController.cs

--- DatingApp/Controllers/AuthController.cs
+++ DatingApp/Controllers/AuthController.cs
@@ -19,8 +19,6 @@ namespace DatingApp.Controllers
     {
         public IAuthRepository _auth { get; }
         public IConfiguration _configuration { get; }
-        public IDatingRepository _repository { get;}
-        public AuthController(IAuthRepository auth, IConfiguration configuration)
         public IMapper _mapper { get; }
 
         public AuthController(IAuthRepository auth, IConfiguration configuration,
