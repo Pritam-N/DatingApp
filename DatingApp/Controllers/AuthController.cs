78f8575efc70260009478c0e4fae3f6857aa1b1d
Arpita Bal <arpitabal8@gmail.com>
2020-02-13 19:08:21 +0530

    Added Utils reference to AuthController

--- DatingApp/Controllers/AuthController.cs
+++ DatingApp/Controllers/AuthController.cs
@@ -10,7 +10,7 @@ using DatingApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
-
+using DatingApp.Utils;
 namespace DatingApp.Controllers
 {
     [Route("api/[controller]")]
