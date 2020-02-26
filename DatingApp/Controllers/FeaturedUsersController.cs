7b3666f1beaf82004c482a210b66ebf7d3f2db7e
bibhu545 <bibhuranjan.545@gmail.com>
2020-02-26 10:39:50 +0530

    Updated responsiveness for home component and fixing errors in featuredusers controller

--- DatingApp/Controllers/FeaturedUsersController.cs
+++ DatingApp/Controllers/FeaturedUsersController.cs
@@ -12,7 +12,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.Controllers
 {
-    [ServiceFilter(typeof(LogUserActivity))]
     [Route("api/[controller]")]
     [ApiController]
     public class FeaturedUsersController : ControllerBase
